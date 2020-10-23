c275d3350f88ea000f151733a83458cc264b38db
M. R. Hossain <rez182@gmail.com>
2020-10-22 23:37:46 -0400

    first test

---
+++ SearchEngine.java
@@ -0,0 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class SearchEngine {
+	public static void main(String[] args) {
+		System.out.println("Your first argument is: "+args[0]);
+	}
+}
